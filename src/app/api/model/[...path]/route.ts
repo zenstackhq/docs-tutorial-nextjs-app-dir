097f0918b4eb2389d5f62af6c5fc1d33081143a1
ymc9 <104139426+ymc9@users.noreply.github.com>
2024-04-04 08:58:18 +0800

    update

--- src/app/api/model/[...path]/route.ts
+++ src/app/api/model/[...path]/route.ts
@@ -1,19 +1,22 @@
-// import { enhance } from "@zenstackhq/runtime";
+import { enhance } from "@zenstackhq/runtime";
 import { NextRequestHandler } from "@zenstackhq/server/next";
-// import { auth } from "~/server/auth";
+import { auth } from "~/server/auth";
 import { db } from "~/server/db";
 
 // create an enhanced Prisma client with user context
 async function getPrisma() {
-  return db;
-  // const session = await auth();
-  // return enhance(
-  //   db,
-  //   {
-  //     user: session?.user?.id ? { id: session.user.id } : undefined,
-  //   },
-  //   { transactionMaxWait: 10000, transactionTimeout: 10000 },
-  // );
+  const session = await auth();
+  return enhance(
+    db,
+    {
+      user: session?.user?.id ? { id: session.user.id } : undefined,
+    },
+    {
+      transactionMaxWait: 10000,
+      transactionTimeout: 10000,
+      logPrismaQuery: true,
+    },
+  );
 }
 
 const handler = NextRequestHandler({ getPrisma, useAppDir: true });
